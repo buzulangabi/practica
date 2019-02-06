759abbe1dd9c06726dc5e76b554ff78f4d59586a
gabriela <gabriela@WS30007>
2019-02-06 17:10:08 +0200

    add customer and product list printers

---
+++ gabi_practica/Printers/CustomerListPrinter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gabi_practica.printers
+{
+    public class CustomerListPrinter
+    {
+        public void PrintCustomers(List<Customer> list)
+        {
+            foreach (Customer customer in list)
+            {
+                Console.WriteLine(CustomerToString(customer));
+                
+
+            }
+        }
+
+        private string CustomerToString(Customer customer)
+        {
+            string s = $"Id: {customer.Id}; Name: {customer.Name}; Age: {customer.Age}; Address: {customer.Address}";
+            return s;
+        
+        }
+
+
+    }
+}
---
+++ gabi_practica/Printers/ProductListPrinter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gabi_practica.printers
+{
+    public class ProductListPrinter
+    {
+        public void PrintProducts(List<Product> list)
+        {
+            foreach (Product product in list)
+            {
+                string customerString = ProductToString(product);
+                Console.WriteLine(customerString);
+            }
+        }
+
+        private string ProductToString(Product product)
+        {
+            string p = $"Id: {product.ID}; Price: {product.Price}; Name: {product.Name}; Type: {product.Type}";
+            return p;
+        }
+    }
+}
