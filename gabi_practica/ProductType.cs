f1e8231f726484ea3b53e40b2afd91df437591ad
gabriela <gabriela@WS30007>
2019-02-06 14:08:02 +0200

    add ProductType

---
+++ gabi_practica/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gabi_practica
+{
+   public enum ProductType
+    {
+        Food,
+        Materials,
+        Books,
+        Software,
+        Hardware,
+    }
+}
