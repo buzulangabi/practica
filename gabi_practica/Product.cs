67a220aa4d23cfe1e89761d274e9ea88c3c68855
gabriela <gabriela@WS30007>
2019-02-06 11:57:10 +0200

    add product class

---
+++ gabi_practica/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gabi_practica
+{
+    class Product
+    {
+        public int ID { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Name { get; set; }
+
+
+
+        public Product(int id, decimal price, string name)
+        {
+            this.ID = id;
+            this.Price = price;
+            this.Name = name;
+
+        }
+    }
+}
---
+++ gabi_practica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gabi_practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
