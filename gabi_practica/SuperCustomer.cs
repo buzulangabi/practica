405b2060a33a0f531a7ff7e8b80f6b8c21950563
gabriela <gabriela@WS30007>
2019-02-06 13:41:13 +0200

    add SuperCustomer

---
+++ gabi_practica/SuperCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gabi_practica
+{
+   public class SuperCustomer:Customer
+    {
+       
+        public decimal Discount { get; set; }
+
+        public SuperCustomer(int id, string name, int age, decimal discount):base(id,name,age)
+        {
+            this.Discount = discount;
+        }
+    }
+}
