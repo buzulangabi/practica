a1041485219b1529d3d236a4d93da1719a0d25c1
gabriela <gabriela@WS30007>
2019-02-06 14:14:07 +0200

    added type

--- gabi_practica/Product.cs
+++ gabi_practica/Product.cs
@@ -7,16 +7,19 @@ namespace gabi_practica
     public class Product
     {
         public int ID { get; set; }
-
+       
         public decimal Price { get; set; }
+
+        public ProductType Type { get; set; }
 
         public string Name { get; set; }
 
-        public Product(int id, decimal price, string name)
+        public Product(int id, decimal price, string name, ProductType type)
         {
             this.ID = id;
             this.Price = price;
             this.Name = name;
+            this.Type = type;
         }
     }
 }
